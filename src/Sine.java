6dddc3662fbced65c4af551ee4357be86943baba
Noah Garcia <33707491+MAjEsTiC715@users.noreply.github.com>
2018-10-25 21:05:51 -0600

    Add files via upload

--- src/Sine.java
+++ src/Sine.java
@@ -5,8 +5,7 @@ public class Sine extends Signal {
 	
 	public float getSample(long us) {
 		us = us % 1000000;
-		double u = Math.toRadians((float)us / 1000.0f);
-		//double k = Math.PI * 2;
+		double u = Math.toRadians((float)us / 2775.0f);
 		double point = Math.sin(u);
 		return (float)point;
 	}
