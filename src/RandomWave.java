7c3d5d1b185429ea086753c055a535bc2818fd71
Noah Garcia <33707491+MAjEsTiC715@users.noreply.github.com>
2018-10-26 21:52:57 -0600

    Add files via upload

--- src/RandomWave.java
+++ src/RandomWave.java
@@ -1,32 +1,42 @@
+import java.util.Arrays;
 import java.util.Random;
 /**
  * Generates random points within the limit of -1 to 1 on the y-axis
  * */
 public class RandomWave extends Signal {
     Random rand = new Random();
-    private float[] point = new float[1000];
+    private float[] point = new float[3000];
     private int element;
-    
+
     public RandomWave() {
-    	randomGenerator();
+        randomGenerator();
+    }
+
+    private void setElement(int e) {
+        this.element = e;
+    }
+
+    private int getElement() {
+        return element;
     }
     @Override
     public float getSample(long us) {
         us = us % 1000000;
         if (us == 0) {
-        	element = 0;
+            setElement(0);
         }
-        if (us != 0) {
-        	element++;
+        if (us > 0) {
+            int e = getElement() + 1;
+            setElement(e);
         }
-        return point[element];
+        return point[getElement()];
     }
-    
-    public void randomGenerator() {
-    	for (int i = 0; i < 1000; i++) {
-    		float y;
-    		y = -1.0f + rand.nextFloat() * (1.0f - -1.0f);
-    		point[i] = y;
-    	}
+    /** Create float array that holds 1000 unique points that will be used for every period*/
+    private void randomGenerator() {
+        for (int i = 0; i < 1000; i++) {
+            float y;
+            y = -1.0f + rand.nextFloat() * (1.0f - -1.0f);
+            point[i] = y;
+        }
     }
 }
